e1dccf75c901bab27beb01d527a95a4a04b42ba7
Ruba-cyber <61930287+Ruba-cyber@users.noreply.github.com>
2020-06-20 20:38:28 +0300

    Adding Loging And Urls

---
+++ finalbustanjiapp/android/app/src/main/kotlin/com/example/finalbustanjiapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.finalbustanjiapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ finalbustanjiapp/lib/Urls/Urls.dart
@@ -0,0 +1,43 @@
+
+class Urls{
+static String servername='http://yarubhost.com/nancy_android/';
+static String login= servername+'login.php';
+static String checkemail=servername+'check_email.php';
+static String createaccount=servername+'create_account.php';
+static String offername=servername+'get_offer_name.php';
+static String searchoffer=servername+'search_offers.php';
+static String searchcaroffer=servername+'search_cars_offers.php';
+static String optionoffername=servername+'getOption_Offer.php';
+static String options=servername+'search_addtional_options.php';
+static String innsurance=servername+'search_insurance_option.php';
+static String urlimagesboutus=servername+'loadImages.php';
+static String companyinfo=servername+'companyInfo.php';
+static String mybooking=servername+'getCar_reservation.php';
+static String feedback=servername+'rating.php';
+static String forgetpassword=servername+'update_password.php';
+static String getcoin=servername+'get_coin.php';
+static String getlocation=servername+'search_location.php';
+static String completefeedback=servername+'complete_rating.php';
+static String updatepersonal=servername+'update_personal_info.php';
+static String searchlocation=servername+'search_location.php';
+static String searchAllcar=servername+'search_cars.php';
+static String searchresname=servername+'reservation_name.php';
+static String searchdate=servername+'reservation_Date.php';
+static String cancelres=servername+'delete_reservation.php';
+static String getinssurance=servername+'getInsurance_reservation.php';
+static String getoption=servername+'getOptions_reservation.php';
+static String contractdata=servername+'getContracts.php';
+static String updatelocation=servername+'updateLocationsInformation.php';
+static String MakeReservation=servername+'make_reservation.php';
+static String makeoption=servername+'make_reservation_options.php';
+static String contactus=servername+'contact_us_info.php';
+static String branchus=servername+'branch_location_info.php';
+static String searchemail=servername+'reservation_Email.php';
+static String makeinnsurace=servername+'make_reservation_insurance.php';
+static String getcategory=servername+'get_category.php';
+static String getyear=servername+'get_year.php';
+static String getpassangerandluggage=servername+'get_luggage_passenger.php';
+static String searchresult=servername+'filter_cars.php';
+
+
+}
